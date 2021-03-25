70870741aa765c850259b523793655b34a190185
choukin <choukin@dingtalk.com>
2021-03-25 17:18:48 +0800

    feat:修改发药单格式

--- chapter03/person/src/PersonalDetails.tsx
+++ chapter03/person/src/PersonalDetails.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import Button from 'reactstrap/lib/Button'
 import Col from 'reactstrap/lib/Col'
 import Row from 'reactstrap/lib/Row'
-import { IPersonState, IProps } from './types';
+import { Database, IPersonState, IProps, IRecordState, PersonalDetailsTableBuilder, PersonRecord, RecordState } from './types';
 import FormValidation from './FormValidation'
 
 export default class PersonalDetails extends React.Component<IProps,IPersonState>{
     private defaultState: Readonly<IPersonState>;
+    private readonly dataLayer:Database<PersonRecord>;
+    private people:IPersonState[];
     constructor(props:IProps) {
         super(props);
         this.defaultState = props.DefaultState;
         this.state = props.DefaultState
+        const tableBuilder :PersonalDetailsTableBuilder = new PersonalDetailsTableBuilder();
+        this.dataLayer = new Database(tableBuilder.Build())
     }
 
     private updateBinding=(event:any)=>{
@@ -59,7 +63,73 @@ export default class PersonalDetails extends React.Component<IProps,IPersonState
             this.setState(this.state)
         })
     }
+    private async DeletePerson(person:string) {
+        const foundPerson = this.people.find((element:IPersonState)=>{
+            return element.PersonId === person
+        }) 
+        if(!foundPerson) {
+            return ;
+        } 
+        const personState :IRecordState = new RecordState()
+        personState.IsActive = false
+        const state = {...foundPerson,...personState}
+        await this.dataLayer.Update(state)
+        this.loadPeople()
+        this.clear()
+    }
+    private clear = ()=>{
+        this.setState(this.defaultState)
+    }
+
+    private delete = (event:any)=>{
+        const person:string = event.target.value;
+        this.DeletePerson(person)
+    }
+    private Save =()=>{
+        if(!this.canSave) {
+            alert('Cannot save this record with missing or incorrect items')
+            return 
+        }
+        const personState:IRecordState = new RecordState()
+        personState.IsActive = true;
+        const state:PersonRecord = {...this.state, ...personState}
+        if(state.PersonId === '') {
+            state.PersonId =  Date.now().toString()
+            this.dataLayer.Create(state)
+            this.loadPeople()
+            this.clear()
+        } else {
+            this.dataLayer.Update(state).then(res=>{
+                this.loadPeople()
+            })
+        }
+    }
+    private setActive = (event:any)=>{
+        const person:string = event.target.value
+        const state = this.people.find((element:IPersonState)=>{
+            return element.PersonId === person
+        })
+
+        if(state){
+            this.setState(state)
+        }
+    }
+
     public render() {
+        let people = null;
+        if(this.people){
+            const _this = this
+            people = this.people.map(function it(p){
+                return (<Row key={p.PersonId} ><Col lg="6"><label>{p.FirstName}{p.LastName}</label></Col>
+                    <Col lg="3">
+                        <Button value={p.PersonId} color="link" onClick={_this.setActive}>编辑</Button>
+                     </Col>   
+                     <Col lg="3">
+                        <Button value={p.PersonId} color="link" onClick={_this.delete}>删除</Button>
+                     </Col>   
+                </Row>)
+            })
+        }
         return (
             <Row>
                 <Col lg="8">
@@ -99,11 +169,14 @@ export default class PersonalDetails extends React.Component<IProps,IPersonState
                         <Col>
                             <Col>
                                 <Row>
+                                    <Col>{people}</Col>
+                                 </Row>   
+                                <Row>
                                     <Col lg="6">
-                                        <Button size="lg" color="success">加载</Button>
+                                        <Button size="lg" color="success" onClick={this.loadPeople}>加载</Button>
                                     </Col>
                                     <Col>
-                                        <Button size="lg" color="info">新人</Button>
+                                        <Button size="lg" color="info" onClick={this.clear}>新人</Button>
                                     </Col>
                                 </Row>
                                 <Row>
